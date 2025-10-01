d026351dccf3900197d85cfa3ac510c9a9cbace8
amr <amrkhaledelsisy@gmail.com>
2025-10-01 16:16:31 +0300

    Finish P3

--- Self learning/test.js
+++ Self learning/test.js
@@ -1,17 +1,6 @@
-let userName = "Elzero";
-console.log(userName.charAt(0).toLowerCase()); // e
-console.log(userName[0].toLowerCase()); // e
-console.log(userName.slice(0,1).toLowerCase()); // e
-console.log(userName.substring(0,1).toLowerCase()); // e
-console.log(userName.substr(0,1).toLowerCase()); // e
-console.log(userName[0].toLowerCase().repeat(3)); 
-
-//2
-let word = "Elzero";
-let letterZ = "z";
-let letterE = "e";
-let letterO = "O";
-
-console.log(word.includes(letterZ)); // True
-console.log(word.startsWith(letterE.toUpperCase())); // True
-console.log(word.endsWith(letterO.toLowerCase())); // True
+var a=20;
+function hehe () {
+  var a = 10;
+} 
+hehe();
+console.log(a);
